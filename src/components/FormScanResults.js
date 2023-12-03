4575b06fd4254617ac244e87da1813bb88a625be
Mike Black <mblack101@ufl.edu>
2023-12-03 15:13:38 +0000

    Add ability for users to download the a report of finished scan results as a tab-separated-values (.tsv) file.

--- src/components/FormScanResults.js
+++ src/components/FormScanResults.js
@@ -4,13 +4,38 @@
 import React from 'react';
 
 export default function FormScanResults({ results }) {
+    const downloadReport = () => {
+        // Convert results to TSV format
+        const tsvContent = results.map(({ destination, status, details }) =>
+            [destination, status, details].join('\t')
+        ).join('\n');
+
+        const tsvHeader = "Destination\tStatus\tDetails\n";
+        const tsvFile = tsvHeader + tsvContent;
+
+        // Create a Blob from the TSV File
+        const blob = new Blob([tsvFile], { type: 'text/tab-separated-values;charset=utf-8;' });
+
+        // Create a link to trigger the download
+        const link = document.createElement('a');
+        const url = URL.createObjectURL(blob);
+        link.href = url;
+        link.download = "scan-results.tsv";
+        link.style.visibility = 'hidden';
+
+        // Append to document and trigger download
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
     return (
         <div>
-        <h2 className="display-4">Scan Results</h2>
-        <p className="lead">View the summary below or download a detailed report.</p>
+            <h2 className="display-4">Scan Results</h2>
+            <p className="lead">View the summary below or download a detailed report.</p>
             <form className="mb-4">
                 <div className="text-center d-flex justify-content-start">
-                    <button type="submit" id="download-button" className="btn btn-light me-3">
+                    <button type="button" id="download-button" className="btn btn-light me-3" onClick={downloadReport}>
                         Download Report
                     </button>
                     <button type="button" id="restart-button" className="btn btn-outline-light border-3">
@@ -18,28 +43,28 @@ export default function FormScanResults({ results }) {
                     </button>
                 </div>
             </form>
-        <div className="mb-3 table-wrapper">
-        <div className="table-responsive">
-        <table className="table">
-        <thead>
-        <tr>
-        <th scope="col">Destination</th>
-        <th scope="col">Status</th>
-        <th scope="col">Details</th>
-        </tr>
-        </thead>
-        <tbody>
-        {results.map(({ destination, status, details }, index) => (
-            <tr key={index}>
-            <td>{destination}</td>
-            <td>{status}</td>
-            <td>{details}</td>
-            </tr>
-        ))}
-        </tbody>
-        </table>
-        </div>
-        </div>
+            <div className="mb-3 table-wrapper">
+                <div className="table-responsive">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Destination</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Details</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {results.map(({ destination, status, details }, index) => (
+                                <tr key={index}>
+                                    <td>{destination}</td>
+                                    <td>{status}</td>
+                                    <td>{details}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 }
